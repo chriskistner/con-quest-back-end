934eefa6e00f8b1bd2f1d8961e0fff587657ea96
Christopher Kistner <chris.kistner0@gmail.com>
2021-07-25 14:07:57 -0700

    Checked and all seed files successfully populated the db, need to figure out why foreign key constraint isn't letting me update/delete though.

--- db/migrations/20210723183837_character_cards.js
+++ db/migrations/20210723183837_character_cards.js
@@ -2,10 +2,8 @@
 exports.up = function(knex) {
     return knex.schema.createTable('character_cards', table => {
         table.increments()
-        table.integer('card_id').notNullable()
-        table.foreign('card_id').references('cards.id')
-        table.integer('character_id').notNullable()
-        table.foreign('character_id').references('characters.id')
+        table.integer('card_id').references('id').inTable('cards')
+        table.integer('character_id').references('id').inTable('characters')
       }); 
 };
 
