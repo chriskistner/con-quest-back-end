54300fba39d7f8d57ca3fbf5231a8762ed7d920b
Christopher Kistner <chris.kistner0@gmail.com>
2021-08-08 19:10:55 -0700

    Filled out controller functions for card routes.

--- src/models/Cards-C.js
+++ src/models/Cards-C.js
@@ -1,13 +1,97 @@
 import models from '../models/cards';
 
-const getAll = (req, res, next) => {
-    const userId = req.params.user_id
-    const deckId = req.params.deck_id
-  
-    models.getAll(userId, deckId)
+const getAllCards = (req, res, next) => {
+
+    models.getAllCards()
       .then(cards => {
         res.send(cards)
       })
-      .catch(next)
-  }
+      .catch(next);
+  };
+
+  const createCard = (req, res, next) => {
+    const newCard = req.body;
+
   
+    models.createCard(newCard)
+      .then(id => {
+        if (!id) {
+          next({ status: 400, error: 'Card creation failed.' })
+  
+        } else {
+          res.status(201).send(card)
+        }
+      })
+      .catch(next);
+  };
+
+  const deleteCard = (req, res, next) => {
+    const cardId = req.params.card_id;
+
+    models.deleteCard(cardId)
+      .then(id => {
+        if (!id) {
+          next({ status: 400, error: 'Unable to delete card' })
+  
+        } else {
+          res.status(201).send(id)
+        }
+      })
+      .catch(next);
+  };
+
+  const updateCard = (req, res, next) => {
+    const cardId = req.params.card_id;
+
+    models.updateCard(cardId)
+      .then(id => {
+        if (!id) {
+          next({ status: 400, error: 'Unable to update card' })
+  
+        } else {
+          res.status(201).send(id)
+        }
+      })
+      .catch(next);
+  };
+
+  const AddCardToCharacter = (req, res, next) => {
+    const cardId = req.params.card_id;
+    const characterId = req.params.character_id;
+
+    models.AddCardToCharacter (cardId, characterId)
+      .then(id => {
+        if (!id) {
+          next({ status: 400, error: 'Unable to add card to Character Deck' })
+  
+        } else {
+          res.status(201).send(id)
+        }
+      })
+      .catch(next);
+  };
+
+  const DropCardFromCharacter = (req, res, next) => {
+    const cardId = req.params.card_id;
+    const characterId = req.params.character_id;
+
+    models.DropCardFromCharacter (cardId, characterId)
+      .then(id => {
+        if (!id) {
+          next({ status: 400, error: 'Unable to drop card from character deck' })
+  
+        } else {
+          res.status(201).send(id)
+        }
+      })
+      .catch(next);
+  };
+
+
+  module.exports = { getAllCards,
+                    createCard,
+                    deleteCard,
+                    updateCard,
+                    AddCardToCharacter,
+                    DropCardFromCharacter 
+                    };
--- src/routes/cards-R.js
+++ src/routes/cards-R.js
@@ -3,8 +3,8 @@ const cardCon = require('../controllers/cards');
 
 router.get('/', cardCon.getAllCards);
 router.post('/', cardCon.createCard);
-router.delete('/', cardCon.DeleteCard);
-router.put('/', cardCon.UpdateCard);
+router.delete('/:card_id', cardCon.DeleteCard);
+router.put('/:card_id', cardCon.UpdateCard);
 
 router.post('/character/:character_id/card/:card_id', cardCon.AddCardToCharacter);
 router.delete('/character/:character_id/card/:card_id', cardCon.DropCardFromCharacter);
